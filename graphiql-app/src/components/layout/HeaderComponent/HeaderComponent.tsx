ba485a32be7f418ec69231a9005ef218a779da0f
Alesia Branavitskaya <alesun4ik@gmail.com>
2023-05-08 01:02:24 +0300

    feat: add header, footer

--- graphiql-app/src/components/layout/HeaderComponent/HeaderComponent.tsx
+++ graphiql-app/src/components/layout/HeaderComponent/HeaderComponent.tsx
@@ -1,9 +1,23 @@
 import './HeaderComponent.scss';
 
 function HeaderComponent() {
+  const isAuth = false;
+
   return (
-    <header>
-      <p>HEADER</p>
+    <header className="header sticky">
+      <img src="src/assets/logo-desktop.svg" alt="logo" className="header__logo desktop" />
+      <img src="src/assets/logo-mobile.svg" alt="logo" className="header__logo mobile" />
+      <div className="header__buttons">
+        {isAuth
+          ?
+          <>
+            <button className="header__buttons-login">Log in</button>
+            <button className="header__buttons-signup">Sign up</button>
+          </>
+          :
+          <button className="header__buttons-start">Get Started</button>
+        }
+      </div>
     </header>
   )
 }
