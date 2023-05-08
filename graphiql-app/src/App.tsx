ca17c2ed8c122403453fe14351c63f9254567ba1
Mari Stakhovskaya <maryia.jegorova@gmail.com>
2023-05-08 13:12:13 +0300

    feat: add page playground

--- graphiql-app/src/App.tsx
+++ graphiql-app/src/App.tsx
@@ -9,6 +9,7 @@ import AuthorizationPage from './pages/AuthorizationPage/AuthorizationPage';
 import LoginComponent from './components/routes/LoginComponent/LoginComponent';
 import RegisterComponent from './components/routes/RegisterComponent/RegisterComponent';
 import ResetComponent from './components/routes/ResetComponent/ResetComponent';
+import Playground from './pages/Playground/Playground';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<RootRoute />}>
           <Route index element={<MainPage />} />
+          <Route path="/graphiql" element={<Playground />} />
           <Route path="/404" element={<NotFoundPage />} />
           <Route path="/authorization" element={<AuthorizationPage />}>
             <Route index path="/authorization/login" element={<LoginComponent />} />
