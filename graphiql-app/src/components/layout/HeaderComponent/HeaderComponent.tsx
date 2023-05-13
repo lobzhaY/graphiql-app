20222e34ee9d06f0f9db228f0d461bed5b6908b8
Alesia Branavitskaya <alesun4ik@gmail.com>
2023-05-14 00:11:44 +0300

    add language package

--- graphiql-app/src/components/layout/HeaderComponent/HeaderComponent.tsx
+++ graphiql-app/src/components/layout/HeaderComponent/HeaderComponent.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 import './HeaderComponent.scss';
+import { useTranslation } from 'react-i18next';
 
 function HeaderComponent() {
   const isAuth = true;
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language: any) => {
+    i18n.changeLanguage(language);
+  };
 
   const header = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState('100px');
+  const [height, setHeight] = useState('120px');
 
   useEffect(() => {
     const handleScroll = () => {
@@ -37,14 +43,25 @@ function HeaderComponent() {
       <div className="header__buttons">
         {isAuth
           ?
-          <button className="header__buttons-start">Get Started</button>
+          <>
+            <button className="header__buttons-start">{t("header.started")}</button>
+            <div className="header__buttons-language">
+              <button onClick={() => changeLanguage("en")}>EN</button>
+              <button onClick={() => changeLanguage("ru")}>RU</button>
+            </div>
+          </>
           :
           <>
-            <button className="header__buttons-login">Log in</button>
-            <button className="header__buttons-signup">Sign up</button>
+            <button className="header__buttons-login">{t("header.login")}</button>
+            <button className="header__buttons-signup">{t("header.signup")}</button>
+            <div className="header__buttons-language">
+              <button onClick={() => changeLanguage("en")}>EN</button>
+              <button onClick={() => changeLanguage("ru")}>RU</button>
+            </div>
           </>
         }
       </div>
+
     </header>
   )
 }
