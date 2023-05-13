2acddef6514cbc02fe9828c16ef3dc610a6fcbdb
Mari Stakhovskaya <maryia.jegorova@gmail.com>
2023-05-13 12:55:08 +0300

    feat: add block varibles/headers

--- graphiql-app/src/pages/Playground/Playground.tsx
+++ graphiql-app/src/pages/Playground/Playground.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './Playground.scss';
 
+
 function Playground() {
   const url = 'https://rickandmortyapi.com/graphql'
     const initialRequest = `query allCh{
@@ -13,7 +14,9 @@ function Playground() {
   const [request, setRequest] = useState(initialRequest)
   const [response, setResponse] = useState('')
   const [variables, setVariables] = useState('')
-  const [activeTab, setActiveTab] = useState('')
+  const [headers, setHeaders] = useState('')
+  const [activeTab, setActiveTab] = useState<string>('')
+  const [changeArrow, setChangeArrow] = useState<boolean>(false)
 
   const makeRequest = async (query:string) => {
         const res = await fetch(url, {
@@ -26,8 +29,25 @@ function Playground() {
     const dataResults =  await res.json().then((data) =>JSON.stringify(data) )
     setResponse(dataResults)
       }
-     const onclickRequestHandler = () => {
+
+    const onclickRequestHandler = () => {
       makeRequest(request)
+     }
+
+     const onClickVariables = () => {
+      setActiveTab('variables')
+      setChangeArrow(true)
+     }
+
+     const onClickHeaders = () => {
+      setActiveTab('headers')
+      setChangeArrow(true)
+     }
+
+     const onClickChangeArrow = () => {
+      setChangeArrow(!changeArrow)
+      changeArrow ?  setActiveTab('') : setActiveTab('variables')
+      
      }
 
   return (
@@ -40,21 +60,25 @@ function Playground() {
      </div>
      <div className='editor-container'>
         <div className='editor-left-container'>
-          <h3 onClick={onclickRequestHandler}>Play</h3>
+          <div onClick={onclickRequestHandler} className='editor-play'>
+            <svg xmlns="http://www.w3.org/2000/svg" height="48"  fill="gray" viewBox="0 96 960 960" width="48"><path d="m392 743 260-169-260-169v338ZM140 896q-24 0-42-18t-18-42V316q0-24 18-42t42-18h680q24 0 42 18t18 42v520q0 24-18 42t-42 18H140Zm0-60h680V316H140v520Zm0 0V316v520Z"/></svg>
+          </div>
         <textarea className='request-container' onChange={(e)=>setRequest(e.target.value)} defaultValue={request}/>
         <div className='variables-wrapper'>
            <div className='variables-header'>
-             <span onClick={()=>setActiveTab('variables')}>Variables</span>
-             <span onClick={()=>setActiveTab('headers')}>Headers</span>
-             <span onClick={()=>setActiveTab('')}>^</span>
+             <span onClick={onClickVariables} className={activeTab === 'variables' ? 'tab-active' : ''}>Variables</span>
+             <span onClick={onClickHeaders} className={activeTab === 'headers' ? 'tab-active' : ''}>Headers</span>
+             <span onClick={onClickChangeArrow}>
+              {changeArrow ? <img src="src/assets/arrow_v.png" alt="arrow"/> :  <img src="src/assets/arrow_down.png" alt="arrow_down"/>}
+              </span>
             </div>
         {activeTab === 'variables' ? <textarea className='variables-container' onChange={(e)=>setVariables(e.target.value)} defaultValue={variables} /> : null}
-        {activeTab === 'headers' ? <textarea className='variables-container' onChange={(e)=>setVariables(e.target.value)} defaultValue={variables} /> : null}
+        {activeTab === 'headers' ? <textarea className='variables-container' onChange={(e)=>setHeaders(e.target.value)} defaultValue={headers} /> : null}
         </div>
         
         </div>
         
-        <textarea className='response-container' onChange={(e)=>setResponse(e.target.value)} defaultValue={response}/>
+        <div className='response-container'>{response}</div>
      </div>
     </div>
   );
