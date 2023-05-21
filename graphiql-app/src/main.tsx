48f6ff9856afa1dbb4f856ba55a7907a127d622b
Yuliya Lobzha <yulia.lobzha@gmail.com>
2023-05-21 15:14:25 +0300

    feat: add schema graphql

--- graphiql-app/src/main.tsx
+++ graphiql-app/src/main.tsx
@@ -9,11 +9,11 @@ import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './components/ErrorBoundary/ErrorFallback.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+ <React.StrictMode>
     <BrowserRouter>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <App />
       </ErrorBoundary>
     </BrowserRouter>
-  </React.StrictMode>
+ </React.StrictMode>
 );
